a5f0d7815cdc10348ab1bd409560cd66ae17c798
Marina <kodola.marina@gmail.com>
2022-11-07 18:29:42 +0200

    Formatted date

--- src/Weather.js
+++ src/Weather.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import FormattedDate from "./FormattedDate";
 import axios from "axios";
 import "./Weather.css";
 
@@ -8,12 +9,13 @@ export default function Weather() {
   function handleResponse(response) {
     setWeather({
       ready: true,
-      temperature: response.data.main.temp,
-      humidity: response.data.main.humidity,
-      description: response.data.weather[0].description,
-      icon: response.data.weather[0].icon,
+      temperature: response.data.temperature.current,
+      humidity: response.data.temperature.humidity,
+      description: response.data.condition.description,
+      icon: response.data.condition.icon_url,
       wind: response.data.wind.speed,
-      city: response.data.name,
+      city: response.data.city,
+      date: new Date(response.data.time * 1000),
     });
   }
 
@@ -41,23 +43,24 @@ export default function Weather() {
         </form>
         <h1>{weather.city}</h1>
         <ul>
-          <li>Friday 20:50</li>
+          <li>
+            <FormattedDate date={weather.date} />
+          </li>
           <li className="text-capitalize">{weather.description}</li>
         </ul>
         <div className="row">
           <div className="col">
-            <img
-              src="https://iconarchive.com/download/i89287/icons8/ios7/Weather-Partly-Cloudy-Rain.ico"
-              alt="weather icon"
-            />{" "}
-            <span className="temperature">{weather.temperature}</span>
+            <img src={weather.icon} alt="weather icon" />{" "}
+            <span className="temperature">
+              {Math.round(weather.temperature)}
+            </span>
             <span className="unit">°C</span>
           </div>
 
           <div className="col">
             <ul>
               <li>Humidity: {weather.humidity}%</li>
-              <li>Wind: {weather.wind} km/h</li>
+              <li>Wind: {Math.round(weather.wind)} km/h</li>
             </ul>
           </div>
         </div>
